b489dd23793ba63474dac13d2bdcfd6c90162464
Coffeebreaks <stefan@grafikcentralen.se>
2022-01-24 19:35:21 +0100

    amost done

--- packages/chimneytec/src/components/page.js
+++ packages/chimneytec/src/components/page.js
@@ -1,15 +1,16 @@
-import React from "react";
+
+import {styled} from "frontity";
 import { connect } from "frontity";
 import Switch from "@frontity/components/switch";
 import CT_Hero from "./acf_blocks/ct_hero";
 import CT_Product_Banner from "./acf_blocks/ct_product_banner";
 import CT_InfoBox from "./acf_blocks/ct_infobox";
-import styled from "frontity";
 import CT_ReferensBanner from "./acf_blocks/ct_referensbanner";
 import CT_InfoPage from "./acf_blocks/ct_infopage"
 import CT_Document from "./acf_blocks/ct_document"
 import ContactForm from "./acf_blocks/contactForm";
 import ContactBlock from "./acf_blocks/ct_contactBlock";
+import CT_Productpage from "./acf_blocks/ct_productpage";
 
 const Page = ({ state }) => {
   const data = state.source.get(state.router.link);
@@ -27,13 +28,13 @@ const Page = ({ state }) => {
               api={x}
             />
             <CT_InfoBox when={x.acf_fc_layout === "infobox"} api={x} />
-
             <CT_ReferensBanner when={x.acf_fc_layout === "referensbanner"} api={x} state={state}/>
             <CT_InfoPage when={x.acf_fc_layout === "infosida"} api={x} state={state}/>
             <CT_Document when={x.acf_fc_layout === "dokument"} api={x} state={state}/>
             <ContactForm when={x.acf_fc_layout === "kontaktformular"} api={x} state={state}/>
-            <ContactBlock when={x.acf_fc_layout === "kontactblock"} api={x}/>
-            
+            <ContactBlock when={x.acf_fc_layout === "kontaktblock"} api={x}/>
+            <CT_Productpage when={x.acf_fc_layout === "produktsida"} api={x}/>
+
           </Switch>
         </>
       ))}
